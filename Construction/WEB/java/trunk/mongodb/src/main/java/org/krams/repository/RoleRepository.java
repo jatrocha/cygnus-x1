8819910f6f611f9a77d5de6f07613c2702a22478
jatrocha@gmail.com <jatrocha@gmail.com@e0b36822-5bc9-7d84-8ce5-7957c8a04205>
2013-02-08 02:14:29 +0000

    excluindo projetos desnecessarios.

--- Construction/WEB/java/trunk/mongodb/src/main/java/org/krams/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package org.krams.repository;
-
-import org.krams.domain.Role;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RoleRepository extends MongoRepository<Role, String> {
-}
--- Construction/WEB/java/trunk/mongodb/src/main/java/org/krams/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.krams.repository;
-
-import org.krams.domain.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-	
-	User findByUsername(String username);
-}
