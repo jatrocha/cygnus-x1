70f15840860e820fe0a90930efa065f9f55fb2c0
jatrocha@gmail.com <jatrocha@gmail.com@e0b36822-5bc9-7d84-8ce5-7957c8a04205>
2013-02-13 18:44:48 +0000

    - Testes Unitários utilizando o MongoDB.

--- Construction/WEB/java/trunk/rest-api/src/test/java/br/com/cygnus/exemplos/business/impl/LivroBusinessTestBase.java
+++ Construction/WEB/java/trunk/rest-api/src/test/java/br/com/cygnus/exemplos/business/impl/LivroBusinessTestBase.java
@@ -1,24 +1,9 @@
 package br.com.cygnus.exemplos.business.impl;
 
 import static br.com.cygnus.framework.IObjetoGenerico.NULL_STRING;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.springframework.data.mongodb.core.MongoTemplate;
-
 import br.com.cygnus.exemplos.commons.dto.LivroDTO;
 import br.com.cygnus.exemplos.commons.dto.LivroFilterDTO;
-import br.com.cygnus.exemplos.helper.InitMongoDB;
 import br.com.cygnus.exemplos.persistence.model.Livro;
-
-import com.mongodb.Mongo;
-
-import de.flapdoodle.embed.mongo.MongodExecutable;
-import de.flapdoodle.embed.mongo.MongodProcess;
-import de.flapdoodle.embed.mongo.MongodStarter;
-import de.flapdoodle.embed.mongo.config.MongodConfig;
-import de.flapdoodle.embed.mongo.distribution.Version;
-import de.flapdoodle.embed.process.runtime.Network;
 
 public abstract class LivroBusinessTestBase {
 
@@ -40,34 +25,34 @@ public abstract class LivroBusinessTestBase {
 
    protected final LivroFilterDTO LIVRO_FILTER_COM_ID = LivroFilterDTO.buildWith(this.ID);
 
-   protected final Livro LIVRO_PARA_LEITURA = new Livro(this.ID, "A Moreninha", "Machado de Assis", "Ficcao");
+   protected final Livro LIVRO_PARA_LEITURA = new Livro(this.ID, "titulo", "autor", "genero");
 
-   private static MongodExecutable mongodExecutable;
-   private static MongodProcess mongodProcess;
-   private static Mongo mongo;
-   private static MongoTemplate mongoTemplate;
-
-   @BeforeClass
-   public static void before() throws Exception {
-
-      MongodStarter runtime = MongodStarter.getDefaultInstance();
-
-      mongodExecutable = runtime.prepare(new MongodConfig(Version.V2_0_6, 27017, Network.localhostIsIPv6()));
-
-      mongodProcess = mongodExecutable.start();
-
-      mongo = new Mongo("localhost", 27017);
-
-      mongoTemplate = new MongoTemplate(mongo, "exemplos");
-
-      new InitMongoDB(mongoTemplate).init();
-   }
-
-   @AfterClass
-   public static void destroy() throws Exception {
-
-      mongodProcess.stop();
-
-      mongodExecutable.stop();
-   }
+   // private static MongodExecutable mongodExecutable;
+   // private static MongodProcess mongodProcess;
+   // private static Mongo mongo;
+   // private static MongoTemplate mongoTemplate;
+   //
+   // @BeforeClass
+   // public static void before() throws Exception {
+   //
+   // MongodStarter runtime = MongodStarter.getDefaultInstance();
+   //
+   // mongodExecutable = runtime.prepare(new MongodConfig(Version.V2_0_6, 27017, Network.localhostIsIPv6()));
+   //
+   // mongodProcess = mongodExecutable.start();
+   //
+   // mongo = new Mongo("localhost", 27017);
+   //
+   // mongoTemplate = new MongoTemplate(mongo, "exemplos");
+   //
+   // new InitMongoDB(mongoTemplate).init();
+   // }
+   //
+   // @AfterClass
+   // public static void destroy() throws Exception {
+   //
+   // mongodProcess.stop();
+   //
+   // mongodExecutable.stop();
+   // }
 }
